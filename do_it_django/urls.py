a47d1a1705e7812b0aaec7596fdac4d4e16eff80
qldrh112 <didie8520@gmail.com>
2024-01-17 19:44:34 +0900

    CBV

--- do_it_django/urls.py
+++ do_it_django/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("blog", include('blog.urls')),
+    path("blog/", include('blog.urls')),
     path("admin/", admin.site.urls),
     path("", include('single_pages.urls')),
 ]
